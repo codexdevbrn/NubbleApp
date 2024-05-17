f80d5ddaaa7c5e73ccf0b3b5bc1a04697f9c1ea2
Bruno Duarte <brunoduarte.inf@outlook.com>
2024-05-17 14:34:33 -0300

    first commit

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import {
+  Text as RNText,
+  TextProps as RNTextProps,
+  TextStyle,
+} from 'react-native';
+
+interface TextProps extends RNTextProps {
+  preset: TextVariants;
+  bold?: boolean;
+  semiBold?: boolean;
+  italic?: boolean;
+}
+
+export function Text({
+  children,
+  preset = 'paragraphMedium',
+  style,
+  bold,
+  semiBold,
+  italic,
+  ...rest
+}: TextProps) {
+  const fontFamily = getFontFamily(bold, italic, semiBold);
+
+  return (
+    /* merge entre o fontSize preset e o style nativo */
+    <RNText style={[$fontSizes[preset], {fontFamily}, style]} {...rest}>
+      {children}
+    </RNText>
+  );
+}
+
+function getFontFamily(bold?: boolean, italic?: boolean, semiBold?: boolean) {
+  switch (true) {
+    case bold && italic:
+      return $fontFamily.boldItalic;
+    case bold:
+      return $fontFamily.bold;
+    case italic:
+      return $fontFamily.italic;
+    case semiBold && italic:
+      return $fontFamily.mediumItalic;
+    case semiBold:
+      return $fontFamily.semiBold;
+    default:
+      return $fontFamily.regular;
+  }
+}
+type TextVariants =
+  | 'headingLarge'
+  | 'headingMedium'
+  | 'headingSmall'
+  | 'paragraphLarge'
+  | 'paragraphMedium'
+  | 'paragraphSmall'
+  | 'paragraphCaption'
+  | 'paragraphCaptionSmall';
+
+/* Record - cria um map entre duas interfaces, a primeira define
+a chave do objeto e a segunda o valor do objeto que vem do TextStyle do React Native */
+const $fontSizes: Record<TextVariants, TextStyle> = {
+  headingLarge: {fontSize: 32, lineHeight: 38.4},
+  headingMedium: {fontSize: 22, lineHeight: 26.4},
+  headingSmall: {fontSize: 18, lineHeight: 23.4},
+
+  paragraphLarge: {fontSize: 18, lineHeight: 25.2},
+  paragraphMedium: {fontSize: 16, lineHeight: 22.4},
+  paragraphSmall: {fontSize: 14, lineHeight: 19.6},
+
+  paragraphCaption: {fontSize: 12, lineHeight: 16.8},
+  paragraphCaptionSmall: {fontSize: 10, lineHeight: 14},
+};
+
+const $fontFamily = {
+  bold: 'Satoshi-Bold',
+  semiBold: 'Satoshi-Medium',
+  regular: 'Satoshi-Regular',
+  italic: 'Satoshi-Italic',
+  boldItalic: 'Satoshi-BoldItalic',
+  mediumItalic: 'Satoshi-MediumItalic',
+};
