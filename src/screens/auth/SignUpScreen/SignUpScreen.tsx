be8a2271967dc8aae810ef49085be436afedb962
Bruno Duarte <brunoduarte.inf@outlook.com>
2024-08-07 12:23:39 -0300

    Feat

--- src/screens/auth/SignUpScreen/SignUpScreen.tsx
+++ src/screens/auth/SignUpScreen/SignUpScreen.tsx
@@ -4,9 +4,15 @@ import {Text} from '../../../components/Text/Text';
 import {TextInput} from '../../../components/TextInput/TextInput';
 import {Button} from '../../../components/Button/Button';
 import {PasswordInput} from '../../../components/PasswordInput/PasswordInput';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../../../routes/Router';
 
-export function SignUpScreen() {
-    function submitForm() {}
+type ScreenProps = NativeStackScreenProps<RootStackParamList, 'SignUpScreen'>;
+
+export function SignUpScreen({navigation}: ScreenProps) {
+    function submitForm() {
+        navigation.navigate('SuccessScreen');
+    }
     return (
         <Screen canGoBack scrollable>
             <Text preset="headingLarge" mb="s32" bold>
---
+++ src/screens/auth/SuccessScreen/SuccessScreen.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Screen} from '../../../components/Screen/Screen';
+import {Text} from '../../../components/Text/Text';
+import {Button} from '../../../components/Button/Button';
+import {Icon} from '../../../components/Icon/Icon';
+
+export function SuccessScreen() {
+    function goBackHome() {
+        //TODO: reset navigation
+    }
+    return (
+        <Screen>
+            <Icon name="bellOn" />
+            <Text preset="headingLarge" bold mt="s24">
+                Title
+            </Text>
+            <Text preset="paragraphLarge" mt="s16">
+                subtitle
+            </Text>
+            <Button onPress={goBackHome} title="Voltar ao início" mt="s40" />
+        </Screen>
+    );
+}
