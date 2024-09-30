3e095a1cd01f285da2a56b7e08cfc6309949fea8
codexdevbrn <brunoduarte.inf@outlook.com>
2024-09-30 20:52:26 -0300

    Stach

--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -3,7 +3,7 @@ import {api, PageAPI} from '@api/index';
 import {PostAPI} from './postTypes';
 
 async function getList(): Promise<PageAPI<PostAPI>> {
-    const response = await api.get<PageAPI<PostAPI>>('/user/post');
+    const response = await api.get<PageAPI<PostAPI>>('user/post');
     return response.data;
 }
 
